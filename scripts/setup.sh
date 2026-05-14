625f3706b0880c50f6629eac093033fc1491bdb3
Nick Galbreath <client9@users.noreply.github.com>
2026-05-14 16:58:05 -0700

    install spaCy

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,5 @@ python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install nltk
-
-#python3 -m spacy download en_core_web_sm
+python3 -m spacy download en_core_web_sm
 
